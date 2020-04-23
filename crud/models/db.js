bbbd3caf1c6c3aeb9077b597220a7bb7db31e2a5
seenam khan <seenamkhan23@gmail.com>
2020-04-23 20:44:28 +0530

    exercise4

--- crud/models/db.js
+++
@@ -1,7 +0,0 @@
-//connection
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/employee',{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log('connected to mongoDb...'))
-.catch(err=>console.error("couldn't connect to mongodb",err));
-
-require('./employee.model');
