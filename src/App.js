36ed460bedc1def1e3b14adcc0e7e47348674b7e
seenam khan <seenamkhan23@gmail.com>
2020-05-04 15:11:06 +0530

    tree structure in react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tree from './Components/tree'
+import "@elastic/eui/dist/eui_theme_light.css";
+function App() {
+  return (
+    <div className="App">
+   <Tree/>
+     
+    </div>
+  );
+}
+
+export default App;
